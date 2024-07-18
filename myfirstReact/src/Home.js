326dcf3f2711fc91966eb6480ffcb4a7ec5ca94b
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-07-18 10:09:54 +0300

    duplicated

---
+++ myfirstReact/src/Home.js
@@ -0,0 +1,11 @@
+// sfc -> Stateless Functional Component
+
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h2>Homepage</h2>
+        </div>
+     );
+}
+ 
+export default Home;
